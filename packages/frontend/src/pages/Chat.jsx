43b126e9f942371d8ba023cef966dd7decd5f3c9
Gabriel Reis <55472396+gabrielsrs@users.noreply.github.com>
2025-07-11 23:20:48 -0300

    chat page

--- packages/frontend/src/pages/Chat.jsx
+++ packages/frontend/src/pages/Chat.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
+
 export function Chat() {
+    const [chatQuantity, setChatQuantity] = useState(0)
+    
     return (
-        <div className="mt-12">
-        <h1>Chat</h1>
+        <div className="grid grid-cols-18 grid-rows-auto gap-4 mt-12 p-4">
+            <div className="h-52 flex col-span-5 pt-14">
+                <div>
+                    <h1 className="text-[114px]/[100px] font-[fantasy] px-0.5">Chat</h1>
+                    <div className="flex justify-between text-sm/2 font-semibold px-1.5">
+                        <span>time capsule</span>
+                        <span>Feed</span>
+                    </div>
+                </div>
+                <div className="text-xl/1 font-semibold ml-1">
+                    <span>[ </span>
+                    <span className="text-base">{chatQuantity}</span>
+                    <span> ]</span>
+                </div>
+            </div>
+            <div className="col-span-13"></div>
         </div>
     )
 }
